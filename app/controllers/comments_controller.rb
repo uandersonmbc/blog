d0560c3684611f1efa62dcc5110e9b65667b8b31
uandersonmbc@gmail.com <uandersonmbc@gmail.com>
2023-07-24 01:20:15 -0300

    Ininitial Commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,40 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "teste", password: "teste123", only: :destroy
+
+  # GET /comments or /comments.json
+  def index
+    @comments = Comment.all
+  end
+
+  # GET /comments/1 or /comments/1.json
+  def show
+  end
+
+  # GET /comments/new
+  def new
+    @comment = Comment.new
+  end
+
+  # GET /comments/1/edit
+  def edit
+  end
+
+  # POST /comments or /comments.json
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
+end
